b880954a85296ada9e6e56bbc8ac1ff06978e7a3
Kevin Jossart <kevin.jossart@kunstmaan.be>
2019-02-08 17:17:56 +0100

    fixes first setup users

--- src/Rest/UserBundle/KunstmaanRestUserBundle.php
+++ src/Rest/UserBundle/KunstmaanRestUserBundle.php
@@ -2,7 +2,7 @@
 
 namespace Kunstmaan\Rest\UserBundle;
 
-use Kunstmaan\Rest\TranslationsBundle\DependencyInjection\Compiler\NelmioDefinitionsCompilerPass;
+use Kunstmaan\Rest\UserBundle\DependencyInjection\Compiler\NelmioDefinitionsCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
