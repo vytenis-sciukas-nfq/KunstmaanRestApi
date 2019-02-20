53491dcefe52fa0e78fa0e501810e3256ef718a5
Kevin Jossart <kevin.jossart@kunstmaan.be>
2019-02-20 18:20:25 +0100

    paginator self links are dynamic and not hardcoded

--- src/Rest/CoreBundle/Helper/Controller/Paginator.php
+++ src/Rest/CoreBundle/Helper/Controller/Paginator.php
@@ -8,9 +8,22 @@ use Pagerfanta\Adapter\AdapterInterface;
 use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class Paginator
 {
+    /** @var RequestStack */
+    private $requestStack;
+
+    /**
+     * Paginator constructor.
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * @param \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder $query
      * @param int $page
@@ -19,11 +32,11 @@ class Paginator
      * @param string $route
      * @return PaginatedRepresentation
      */
-    public function getPaginatedQueryBuilderResult($query, $page = 0, $limit = 20, \Closure $closure = null, $route = 'get_nodes')
+    public function getPaginatedQueryBuilderResult($query, $page = 0, $limit = 20, \Closure $closure = null)
     {
         $adapter = new DoctrineORMAdapter($query);
 
-        return $this->getPaginatedRepresentation($adapter, $page, $limit, $closure, $route);
+        return $this->getPaginatedRepresentation($adapter, $page, $limit, $closure);
     }
 
     /**
@@ -34,11 +47,11 @@ class Paginator
      * @param string $route
      * @return PaginatedRepresentation
      */
-    public function getPaginatedArrayResult(array $array, $page = 0, $limit = 20, \Closure $closure = null, $route = 'get_nodes')
+    public function getPaginatedArrayResult(array $array, $page = 0, $limit = 20, \Closure $closure = null)
     {
         $adapter = new ArrayAdapter($array);
 
-        return $this->getPaginatedRepresentation($adapter, $page, $limit, $closure, $route);
+        return $this->getPaginatedRepresentation($adapter, $page, $limit, $closure);
     }
 
     /**
@@ -48,10 +61,15 @@ class Paginator
      * @param int $limit
      * @return PaginatedRepresentation
      */
-    protected function getPaginatedRepresentation(AdapterInterface $adapter, $page = 0, $limit = 20, \Closure $closure = null, $route = 'get_nodes') {
+    protected function getPaginatedRepresentation(AdapterInterface $adapter, $page = 0, $limit = 20, \Closure $closure = null) {
         $pagerfanta = new Pagerfanta($adapter);
         $pagerfanta->setMaxPerPage($limit);
         $pagerfanta->setCurrentPage($page);
+        $request = $this->requestStack->getCurrentRequest();
+        $route = 'get_nodes';
+        if(null !== $request) {
+            $route = $request->get('_route');
+        }
 
         $items = $pagerfanta->getCurrentPageResults();
         if (null !== $closure) {
