7e99142424e1c57dcb3c17d9f92c407c130e98bf
Kevin Jossart <kevin.jossart@kunstmaan.be>
2019-04-11 16:27:00 +0200

    Redirect bundle added

--- src/Rest/RedirectBundle/Controller/RedirectController.php
+++ src/Rest/RedirectBundle/Controller/RedirectController.php
@@ -1,48 +1,41 @@
 <?php
 
-namespace Kunstmaan\Rest\ConfigBundle\Controller;
+namespace Kunstmaan\Rest\RedirectBundle\Controller;
 
 use Doctrine\Common\Persistence\ObjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\ControllerTrait;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Kunstmaan\ConfigBundle\Entity\AbstractConfig;
+use Kunstmaan\RedirectBundle\Entity\Redirect;
 use Swagger\Annotations as SWG;
 use Hateoas\Representation\PaginatedRepresentation;
 use Kunstmaan\FormBundle\Entity\FormSubmission;
 use FOS\RestBundle\Controller\Annotations\View;
 use Kunstmaan\Rest\CoreBundle\Controller\AbstractApiController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 
-/**
- */
-class ConfigController extends AbstractApiController
+class RedirectController extends AbstractApiController
 {
     use ControllerTrait;
 
     /** @var EntityManagerInterface */
     private $doctrine;
 
-    /** @var array */
-    private $configuration;
-
-    public function __construct(EntityManagerInterface $doctrine, array $configuration)
+    public function __construct(EntityManagerInterface $doctrine)
     {
         $this->doctrine = $doctrine;
-        $this->configuration = $configuration;
     }
 
     /**
      * Retrieve form submissions paginated
      *
      * @SWG\Get(
-     *     path="/api/config/{internalName}",
-     *     description="Get config by internal name",
-     *     operationId="getConfig",
+     *     path="/api/redirects",
+     *     description="Get all redirects",
+     *     operationId="getRedirects",
      *     produces={"application/json"},
-     *     tags={"config"},
+     *     tags={"redirect"},
      *     @SWG\Parameter(
      *         name="X-Api-Key",
      *         in="header",
@@ -55,6 +48,20 @@ class ConfigController extends AbstractApiController
      *         description="Returned when successful",
      *         @SWG\Schema(ref="#/definitions/singleConfig")
      *     ),
+     *     @SWG\Parameter(
+     *         name="page",
+     *         in="query",
+     *         type="integer",
+     *         description="The current page",
+     *         required=false,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         type="integer",
+     *         description="Amount of results (default 20)",
+     *         required=false,
+     *     ),
      *     @SWG\Response(
      *         response=403,
      *         description="Returned when the user is not authorized to fetch media",
@@ -66,31 +73,23 @@ class ConfigController extends AbstractApiController
      *         @SWG\Schema(ref="#/definitions/ErrorModel")
      *     )
      * )
+     * @QueryParam(name="page", nullable=false, default="1", requirements="\d+", description="The current page")
+     * @QueryParam(name="limit", nullable=false, default="20", requirements="\d+", description="Amount of results")
      *
-     * @Rest\Get("/config/{internalName}")
+     * @Rest\Get("/redirects")
      * @View(statusCode=200)
      *
-     * @return AbstractConfig|Response
+     * @return PaginatedRepresentation
      */
-    public function getConfigAction(string $internalName)
+    public function getRedirectsAction(ParamFetcherInterface $paramFetcher)
     {
-        /** @var ObjectRepository $repository */
-        $repository = $this->doctrine->getRepository(FormSubmission::class);
+        $page = $paramFetcher->get('page');
+        $limit = $paramFetcher->get('limit');
 
+        /** @var ObjectRepository $repository */
+        $repository = $this->doctrine->getRepository(Redirect::class);
         $result = $repository->findAll();
 
-        foreach ($this->configuration['entities'] as $class) {
-            $entity = new $class();
-
-            if ($entity->getInternalName() == $internalName) {
-                $repo = $this->doctrine->getRepository($class);
-                /** @var AbstractConfig $config */
-                $config = $repo->findOneBy([]);
-
-                return $config;
-            }
-        }
-
-        return new NotFoundHttpException("not found");
+        return $this->getPaginator()->getPaginatedArrayResult($result, $page, $limit);
     }
 }
---
+++ src/Rest/RedirectBundle/KunstmaanRestRedirectBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kunstmaan\Rest\RedirectBundle;
+
+use Kunstmaan\Rest\RedirectBundle\DependencyInjection\Compiler\NelmioDefinitionsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KunstmaanRestRedirectBundle extends Bundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new NelmioDefinitionsCompilerPass());
+    }
+}
